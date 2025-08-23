ee517e6a558cb2b68fdf11531adc181e5e61ff7b
Jesse Smith <jesses2025smith@gmail.com>
2025-08-24 03:55:42 +0800

    add opus path

--- build.rs
+++ build.rs
@@ -2,13 +2,14 @@ use std::{env, path};
 
 fn main() {
     let root = path::PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let opus_path = root.join("opus");
 
     let bindings = bindgen::Builder::default()
-        // .header(root.join("opus/include/opus_custom.h").to_string_lossy().into_owned())
-        .header(root.join("opus/include/opus_multistream.h").to_string_lossy().into_owned())
-        // .header(root.join("opus/include/opus_projection.h").to_string_lossy().into_owned())
-        .clang_arg("-I").clang_arg(root.join("opus/include").to_string_lossy())
-        .clang_arg("-I").clang_arg(root.join("opus/src").to_string_lossy())
+        .header(opus_path.join("include/opus_custom.h").to_string_lossy().into_owned())
+        .header(opus_path.join("include/opus_multistream.h").to_string_lossy().into_owned())
+        .header(opus_path.join("include/opus_projection.h").to_string_lossy().into_owned())
+        .clang_arg("-I").clang_arg(opus_path.join("include").to_string_lossy())
+        .clang_arg("-I").clang_arg(opus_path.join("src").to_string_lossy())
         .derive_debug(true)
         .derive_default(true)
         // .allowlist_recursively(false)
